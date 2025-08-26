f70e1ef54070dff4c623a1844ee3b3dfac9bf789
MethumW <methumweerasinghe16@gmail.com>
2025-08-27 00:06:50 +0530

    add run animation

--- 1.js
+++ 1.js
@@ -1,26 +1,40 @@
 var robo = document.getElementById("robo");
-let idleImageNumber = 0;
-let idleAnimationNumber = 0;
-
+let idleImageNumber = 1;
+let idleAnimationInterval = 0;
 
 function idleAnimation() {
-   
-idleImageNumber = idleImageNumber + 1;
-    
-
- 
-
-   if(idleImageNumber == 11){
-        idleImageNumber =  1;
+    robo.src = "resources/Idle (" + idleImageNumber + ").png";
+    idleImageNumber++;
+    if (idleImageNumber > 10) {
+        idleImageNumber = 1;
     }
-
-robo.src = "resources/Idle (" + idleImageNumber + ").png";    
-
-
-  }
-
-
-function idleAnimationStart() {
-    idleAnimationNumber = setInterval(idleAnimation,200);
 }
 
+function idleAnimationStart() {
+    idleAnimationInterval = setInterval(idleAnimation, 200);
+}
+
+let runImageNumber = 1;
+let runAnimationInterval = 0;
+
+function runAnimation() {
+    robo.src = "resources/Run (" + runImageNumber + ").png";
+    runImageNumber++;
+    if (runImageNumber == 8) {
+        runImageNumber = 1;
+    }
+}
+
+function runAnimationStart() {
+    clearInterval(idleAnimationInterval);
+    runAnimationInterval = setInterval(runAnimation, 100);
+}
+
+function keycheck(event) {
+    var keycode = event.which;
+    if (keycode == 13) {
+        if (runAnimationInterval === 0) {
+            runAnimationStart();
+        }
+    }
+}
